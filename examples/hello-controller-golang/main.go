2e0682a00ba029137cec8386e2451ab17eb7cfbc
Moto Ishizawa <summerwind.jp@gmail.com>
2019-07-09 20:51:58 +0900

    Refactor to use resource centric config

--- examples/hello-controller-golang/main.go
+++ examples/hello-controller-golang/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -9,7 +8,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	kconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -17,6 +18,8 @@ import (
 
 	"github.com/summerwind/whitebox-controller/config"
 	"github.com/summerwind/whitebox-controller/manager"
+	"github.com/summerwind/whitebox-controller/reconciler/state"
+	"github.com/summerwind/whitebox-controller/webhook"
 )
 
 type Hello struct {
@@ -39,48 +42,71 @@ type State struct {
 	Object Hello `json:"object"`
 }
 
-type HelloHandler struct{}
+type Handler struct{}
 
-func (h HelloHandler) Run(buf []byte) ([]byte, error) {
-	state := State{}
+func (h *Handler) HandleState(ss *state.State) error {
+	s := State{}
 
-	err := json.Unmarshal(buf, &state)
+	err := state.Unpack(ss, &s)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	if state.Object.Status.Phase != "completed" {
-		log.Printf("message: %s", state.Object.Spec.Message)
-		state.Object.Status.Phase = "completed"
+	if s.Object.Status.Phase != "completed" {
+		log.Printf("message: %s", s.Object.Spec.Message)
+		s.Object.Status.Phase = "completed"
 	}
 
-	out, err := json.Marshal(&state)
+	err = state.Pack(&s, ss)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return out, nil
+	return nil
+}
+
+func (h *Handler) HandleAdmissionRequest(req admission.Request) (admission.Response, error) {
+	hello := &Hello{}
+
+	err := webhook.Unpack(req.Object, hello)
+	if err != nil {
+		return admission.Response{}, err
+	}
+
+	if hello.Spec.Message == "" {
+		return admission.Denied("message must be specified"), nil
+	}
+
+	return admission.Allowed(""), nil
 }
 
 func main() {
-	handler := HelloHandler{}
+	logf.SetLogger(logf.ZapLogger(false))
 
 	c := &config.Config{
-		Controllers: []*config.ControllerConfig{
-			&config.ControllerConfig{
-				Name: "hello-controller",
-				Resource: schema.GroupVersionKind{
-					Group:   "whitebox.summerwind.github.io",
+		Resources: []*config.ResourceConfig{
+			{
+				GroupVersionKind: schema.GroupVersionKind{
+					Group:   "whitebox.summerwind.dev",
 					Version: "v1alpha1",
 					Kind:    "Hello",
 				},
 				Reconciler: &config.ReconcilerConfig{
 					HandlerConfig: config.HandlerConfig{
-						Func: &config.FuncHandlerConfig{
-							Handler: handler,
-						},
+						StateHandler: &Handler{},
 					},
 				},
+				Validator: &config.HandlerConfig{
+					AdmissionRequestHandler: &Handler{},
+				},
+			},
+		},
+		Webhook: &config.ServerConfig{
+			Host: "0.0.0.0",
+			Port: 443,
+			TLS: &config.TLSConfig{
+				CertFile: "/etc/tls/tls.crt",
+				KeyFile:  "/etc/tls/tls.key",
 			},
 		},
 	}
